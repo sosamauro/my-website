78d19efd4d5b72b72994cc8183d710105933ae24
Mauro Sosa <mauro4sosa@gmail.com>
2022-07-19 20:47:02 -0300

    SEO added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import { NextSeo } from 'next-seo';
 
 import Layout from '../src/layout/index';
 import theme from '../src/theme';
@@ -8,6 +9,7 @@ import '../src/theme/styles.css';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <NextSeo title="Mauro Sosa" description="Full Stack Developer" />
       <Layout>
         <Component {...pageProps} />
       </Layout>
